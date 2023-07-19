58961f4a8911efc42e4efc83980cc47acdaeaf5a
Victor <victorkiariem@gmail.com>
2023-07-19 09:29:47 +0300

    fixed the ban user function

--- User_Management/website/main/views.py
+++ User_Management/website/main/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm, PostForm
-from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
 from .models import Post
+from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import User, Group
-# Create your views here.
+# Create views here
 
 
 @login_required(login_url="/login")
@@ -23,15 +23,16 @@ def home(request):
             user = User.objects.filter(id=user_id).first()
             if user and request.user.is_staff:
                 try:
-                    group = Group.ojects.get(name='default')
+                    group = Group.objects.get(name='default')
                     group.user_set.remove(user)
                 except:
                     pass
                 try:
-                    group = Group.ojects.get(name='mod')
+                    group = Group.objects.get(name='mod')
                     group.user_set.remove(user)
                 except:
                     pass
+
     return render(request, "main/home.html", {'posts': posts})
 
 
@@ -49,6 +50,7 @@ def sign_up(request):
 
 
 @login_required(login_url='/login')
+@permission_required("main.add_post", login_url="/login", raise_exception=True)
 def create_post(request):
     if request.method == "POST":
         form = PostForm(request.POST)
